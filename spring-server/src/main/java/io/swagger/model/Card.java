05a47c5b3597013e5d67a6abad103771c1773739
s <s@Z690.localdomain>
2023-08-29 11:58:36 -0600

    Generate server stubs with modified yaml

---
+++ spring-server/src/main/java/io/swagger/model/Card.java
@@ -0,0 +1,180 @@
+package io.swagger.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * Card
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2023-08-29T17:28:16.715952712Z[GMT]")
+
+
+public class Card   {
+  @JsonProperty("first_name")
+  private String firstName = null;
+
+  @JsonProperty("last_name")
+  private String lastName = null;
+
+  @JsonProperty("card_number")
+  private String cardNumber = null;
+
+  @JsonProperty("expiration")
+  private Long expiration = null;
+
+  @JsonProperty("cvc")
+  private String cvc = null;
+
+  public Card firstName(String firstName) {
+    this.firstName = firstName;
+    return this;
+  }
+
+  /**
+   * Get firstName
+   * @return firstName
+   **/
+  @Schema(required = true, description = "")
+      @NotNull
+
+    public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public Card lastName(String lastName) {
+    this.lastName = lastName;
+    return this;
+  }
+
+  /**
+   * Get lastName
+   * @return lastName
+   **/
+  @Schema(required = true, description = "")
+      @NotNull
+
+    public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public Card cardNumber(String cardNumber) {
+    this.cardNumber = cardNumber;
+    return this;
+  }
+
+  /**
+   * Get cardNumber
+   * @return cardNumber
+   **/
+  @Schema(required = true, description = "")
+      @NotNull
+
+  @Pattern(regexp="^(?:4[0-9]{12}(?:[0-9]{3})? |(?:5[1-5][0-9]{2}| 222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12} |3[47][0-9]{13} |3(?:0[0-5]|[68][0-9])[0-9]{11} |6(?:011|5[0-9]{2})[0-9]{12} |(?:2131|1800|35\\d{3})\\d{11} )$")   public String getCardNumber() {
+    return cardNumber;
+  }
+
+  public void setCardNumber(String cardNumber) {
+    this.cardNumber = cardNumber;
+  }
+
+  public Card expiration(Long expiration) {
+    this.expiration = expiration;
+    return this;
+  }
+
+  /**
+   * Get expiration
+   * @return expiration
+   **/
+  @Schema(required = true, description = "")
+      @NotNull
+
+    public Long getExpiration() {
+    return expiration;
+  }
+
+  public void setExpiration(Long expiration) {
+    this.expiration = expiration;
+  }
+
+  public Card cvc(String cvc) {
+    this.cvc = cvc;
+    return this;
+  }
+
+  /**
+   * Get cvc
+   * @return cvc
+   **/
+  @Schema(required = true, description = "")
+      @NotNull
+
+  @Pattern(regexp="^\\d{3}$")   public String getCvc() {
+    return cvc;
+  }
+
+  public void setCvc(String cvc) {
+    this.cvc = cvc;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Card card = (Card) o;
+    return Objects.equals(this.firstName, card.firstName) &&
+        Objects.equals(this.lastName, card.lastName) &&
+        Objects.equals(this.cardNumber, card.cardNumber) &&
+        Objects.equals(this.expiration, card.expiration) &&
+        Objects.equals(this.cvc, card.cvc);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(firstName, lastName, cardNumber, expiration, cvc);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Card {\n");
+    
+    sb.append("    firstName: ").append(toIndentedString(firstName)).append("\n");
+    sb.append("    lastName: ").append(toIndentedString(lastName)).append("\n");
+    sb.append("    cardNumber: ").append(toIndentedString(cardNumber)).append("\n");
+    sb.append("    expiration: ").append(toIndentedString(expiration)).append("\n");
+    sb.append("    cvc: ").append(toIndentedString(cvc)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
